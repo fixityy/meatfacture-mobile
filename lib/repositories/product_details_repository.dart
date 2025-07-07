e37420349d02c960ca749eeab1341d82b7e93339
JorikFat <jorikfat@gmail.com>
2025-07-07 03:53:05 +0300

    fix: repositories

--- lib/repositories/product_details_repository.dart
+++ lib/repositories/product_details_repository.dart
@@ -1,8 +1,7 @@
-import 'package:flutter/material.dart';
 import 'package:smart/services/services.dart';
 
 class ProductDetailsRepository {
-  getProductDeatilsFromRepository({@required String uuid}) async {
+  getProductDeatilsFromRepository({required String uuid}) async {
     ProductDetailsProvider _productDetailsProvider =
         ProductDetailsProvider(uuid: uuid);
     return await _productDetailsProvider.getProductDetailsResponse();
