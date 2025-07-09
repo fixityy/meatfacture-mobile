9c5918303e68ead4bf6a3b30c4aad38a6a12facf
JorikFat <jorikfat@gmail.com>
2025-07-09 23:34:23 +0300

    change: api key

--- lib/services/user_x.dart
+++ lib/services/user_x.dart
@@ -1,6 +1,6 @@
 import 'package:userx_flutter/userx_flutter.dart';
 
-const String USERX_KEY = '433c2016-65f9-46a3-8c78-3b1d8fac511c';
+const String USERX_KEY = '0d2a4337-f63a-4d17-9bea-fa1ecc198a4b';
 
 void initUserX() {
   UserX.start(USERX_KEY);
